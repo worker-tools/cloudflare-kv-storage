8fd28e7dcab6055d09e051c2e18ffa78020fbf84
Florian Klampfer <mail@qwtel.com>
2021-02-17 10:32:40 +0700

    Add default types to interface

--- src/interface.ts
+++ src/interface.ts
@@ -17,14 +17,14 @@ declare var StorageArea: {
  *   This way users can provide extra data to the underlying implementation without type casting.
  */
 export interface StorageArea<BS = any> {
-  set<T>(key: AllowedKey, value: T, opts?: Options): Promise<void> ;
-  get<T>(key: AllowedKey, opts?: Options): Promise<T> ;
+  set<T = any>(key: AllowedKey, value: T, opts?: Options): Promise<void> ;
+  get<T = any>(key: AllowedKey, opts?: Options): Promise<T> ;
   delete(key: AllowedKey, opts?: Options): Promise<void> ;
   clear(opts?: Options): Promise<void> ;
 
   keys(opts?: Options): AsyncIterableIterator<Key>;
-  values<T>(opts?: Options): AsyncIterableIterator<T>;
-  entries<T>(opts?: Options): AsyncIterableIterator<[Key, T]>;
+  values<T = any>(opts?: Options): AsyncIterableIterator<T>;
+  entries<T = any>(opts?: Options): AsyncIterableIterator<[Key, T]>;
 
   backingStore(): BS;
 };
